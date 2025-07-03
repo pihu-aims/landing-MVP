ab32ef937b2be776b8cec5aa3dec55d2e538d554
Wallace-Degamo-AIMS <wallace.degamo@aims.team>
2025-07-03 22:12:21 +0100

    Did BackgroundLayer2.jsx

--- app/page.js
+++ app/page.js
@@ -1,28 +1,37 @@
 // app/page.js
 import ImagesLayer from '../components/layers/ImagesLayer';
 import TextLayer from '../components/layers/TextLayer';
+import BackgroundLayerOne from '../components/layers/BackgroundLayerOne';
+import BackgroundLayerTwo from '../components/layers/BackgroundLayerTwo';
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
       {/* First section background - only covers first section */}
       <div className="fixed top-0 left-0 w-full h-screen z-0">
-        <img
-          className="w-full h-full object-cover"
-          alt="Thumbnail background"
-          src="/images/thumbnail-3-3.png"
-          loading="eager"
-        />
+        {/*<img*/}
+        {/*  className="w-full h-full object-cover"*/}
+        {/*  alt="Thumbnail background"*/}
+        {/*  src="/images/thumbnail-3-3.png"*/}
+        {/*  loading="eager"*/}
+        {/*/>*/}
+          <BackgroundLayerOne />
+          <BackgroundLayerTwo />
       </div>
       
       {/* Content Container - will scroll over the fixed background */}
-      <div className="relative z-10 w-full">
-        {/* Images Layer */}
-        <ImagesLayer />
+        <div className="relative z-10 w-full h-screen">
+            {/* Images Layer */}
+            <div className="absolute inset-0">
+                <ImagesLayer />
+            </div>
 
-        {/* Text Layer */}
-        <TextLayer />
-      </div>
+            {/* Text Layer */}
+            <div className="absolute inset-0">
+                <TextLayer />
+            </div>
+        </div>
+
     </div>
   );
 }
--- components/layers/ImagesLayer.jsx
+++ components/layers/ImagesLayer.jsx
@@ -8,31 +8,31 @@ const imageData = [
     id: "image-1",
     src: "/images/a.png",
     alt: "Image 1",
-    className: "absolute w-[120px] h-[120px] top-[247px] right-[300px] object-cover rounded-lg shadow-md",
+    className: "absolute w-[200px] h-[200px] top-[247px] right-[300px] object-cover rounded-lg shadow-md",
   },
   {
     id: "image-2",
     src: "/images/b.png",
     alt: "Image 2",
-    className: "absolute w-[150px] h-[120px] top-[982px] left-[236px] object-cover rounded-lg shadow-md",
+    className: "absolute w-[230px] h-[200px] top-[982px] left-[236px] object-cover rounded-lg shadow-md",
   },
   {
     id: "image-3",
     src: "/images/c.png",
     alt: "Image 3",
-    className: "absolute w-[120px] h-[120px] top-[1560px] right-[250px] object-cover rounded-lg shadow-md",
+    className: "absolute w-[200px] h-[200px] top-[1560px] right-[250px] object-cover rounded-lg shadow-md",
   },
   {
     id: "image-4",
     src: "/images/d.png",
     alt: "Image 4",
-    className: "absolute w-[120px] h-[120px] top-[1809px] left-[88px] object-cover rounded-lg shadow-md",
+    className: "absolute w-[200px] h-[200px] top-[1809px] left-[88px] object-cover rounded-lg shadow-md",
   },
   {
     id: "image-5",
     src: "/images/e.png",
     alt: "Image 5",
-    className: "absolute w-[120px] h-[150px] top-[2313px] left-[230px] object-cover rounded-lg shadow-md",
+    className: "absolute w-[200px] h-[230px] top-[2313px] left-[230px] object-cover rounded-lg shadow-md",
   },
 ];
 
