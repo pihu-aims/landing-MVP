8b39d17316d14a4e98a9db39daebc6388c8389b2
Wallace-Degamo-AIMS <wallace.degamo@aims.team>
2025-07-03 22:25:28 +0100

    Fixed the two image layers

--- app/page.js
+++ app/page.js
@@ -7,31 +7,24 @@ import BackgroundLayerTwo from '../components/layers/BackgroundLayerTwo';
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
-      {/* First section background - only covers first section */}
-      <div className="fixed top-0 left-0 w-full h-screen z-0">
-        {/*<img*/}
-        {/*  className="w-full h-full object-cover"*/}
-        {/*  alt="Thumbnail background"*/}
-        {/*  src="/images/thumbnail-3-3.png"*/}
-        {/*  loading="eager"*/}
-        {/*/>*/}
-          <BackgroundLayerOne />
-          <BackgroundLayerTwo />
+      {/* Background Layers - stacked vertically */}
+      <div className="relative w-full z-0">
+        <BackgroundLayerOne />
+        <BackgroundLayerTwo />
       </div>
-      
-      {/* Content Container - will scroll over the fixed background */}
-        <div className="relative z-10 w-full h-screen">
-            {/* Images Layer */}
-            <div className="absolute inset-0">
-                <ImagesLayer />
-            </div>
 
-            {/* Text Layer */}
-            <div className="absolute inset-0">
-                <TextLayer />
-            </div>
+      {/* Content Container - will scroll over the background layers */}
+      <div className="absolute top-0 left-0 z-10 w-full h-screen">
+        {/* Images Layer */}
+        <div className="absolute inset-0">
+          <ImagesLayer />
         </div>
 
+        {/* Text Layer */}
+        <div className="absolute inset-0">
+          <TextLayer />
+        </div>
+      </div>
     </div>
   );
 }
--- components/layers/BackgroundLayerOne.jsx
+++ components/layers/BackgroundLayerOne.jsx
@@ -4,21 +4,13 @@ import React from "react";
 
 export default function BackgroundLayerOne() {
   return (
-    <div className="bg-transparent flex justify-center w-full h-full">
-      <div className="w-[1509px] h-[4096px] relative">
-        <div className="absolute w-[1440px] h-[1394px] rounded-lg border shadow-sm">
-          <div className="p-0">
-            <div style={{ position: 'relative', width: '100%', paddingBottom: `${(1394/1440)*100}%` }}>
-              <img
-                className="absolute w-full h-full object-cover"
-                alt="Thumbnail background"
-                src="/images/thumbnail-3-3.png"
-                loading="eager"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="w-full">
+      <img
+        className="w-full h-auto"
+        alt="Thumbnail background"
+        src="/images/thumbnail-3-3.png"
+        loading="eager"
+      />
     </div>
   );
 }
