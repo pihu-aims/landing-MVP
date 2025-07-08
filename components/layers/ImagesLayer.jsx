1140dcf81c408c2352f8c52330483f8ebd8357c5
megan.u06 <meganullahs@gmail.com>
2025-07-08 11:24:36 +0100

    remove image fade

--- components/layers/ImagesLayer.jsx
+++ components/layers/ImagesLayer.jsx
@@ -14,15 +14,22 @@ export default function ImagesLayer() {
 
   if (!isClient) return null;
   
+  // Common style for all images - no fading
+  const imageStyle = {
+    opacity: 1
+  };
+  
   return (
     <div 
       className="fixed top-0 left-0 w-full h-full z-25 pointer-events-none"
+      style={{ opacity: 1 }}
     >
       {/* Image A - First section */}
       <img 
         src="/images/a.png"
         alt="Image A - Our Vision"
         className="absolute w-[400px] h-auto top-[20vh] right-[10vw] object-cover rounded-3xl shadow-lg"
+        style={imageStyle}
       />
       
       {/* Image B - Second section */}
@@ -30,6 +37,7 @@ export default function ImagesLayer() {
         src="/images/b.png"
         alt="Image B - What We've Built"
         className="absolute w-[400px] h-auto top-[120vh] left-[10vw] object-cover rounded-3xl shadow-lg"
+        style={imageStyle}
       />
       
       {/* Image C - Third section */}
@@ -37,6 +45,7 @@ export default function ImagesLayer() {
         src="/images/c.png"
         alt="Image C - Who It's For"
         className="absolute w-[350px] h-auto top-[220vh] right-[10vw] object-cover rounded-3xl shadow-lg"
+        style={imageStyle}
       />
       
       {/* Image D - Fourth section */}
@@ -44,6 +53,7 @@ export default function ImagesLayer() {
         src="/images/d.png"
         alt="Image D - Who We Are"
         className="absolute w-[380px] h-auto top-[320vh] left-[10vw] object-cover rounded-3xl shadow-lg"
+        style={imageStyle}
       />
       
       {/* Image E - Fifth section */}
@@ -51,6 +61,7 @@ export default function ImagesLayer() {
         src="/images/e.png"
         alt="Image E - Additional Content"
         className="absolute w-[450px] h-auto top-[420vh] left-[calc(50%-225px)] object-cover rounded-3xl shadow-lg"
+        style={imageStyle}
       />
     </div>
   );
